3f16223ceb3ffe0460658d27ea54d32de1d1f5ee
Evgenii Nasyrov <inasyrov@yandex.ru>
2016-06-10 16:45:44 +0100

    Modify example page brick

--- examples/bricks/page/page.php
+++ examples/bricks/page/page.php
@@ -10,8 +10,33 @@ $engine->registerMatcher('page', function (ContextInterface $context) {
         '<!DOCTYPE html>',
         (new Context)->tag('html')->cls(['ua_js_no'])->content(
             (new Context)->elem('head')->content(
-                (new Context)->elem('meta')->attrs('charset', 'utf-8')
+                (new Context)->elem('meta')->attrs('charset', 'utf-8'),
+                (new Context)->elem('meta')->attrs([
+                    'http-equiv' => 'X-UA-Compatible',
+                    'content'    => 'IE=edge',
+                ])
             )
         )
     ];
+});
+
+$engine->registerMatcher('page__head', function (ContextInterface $context) {
+    $context->bem(false)
+            ->tag('head');
+});
+
+$engine->registerMatcher('page__meta', function (ContextInterface $context) {
+    $context->bem(false)
+            ->tag('meta');
+});
+
+$engine->registerMatcher('page__link', function (ContextInterface $context) {
+    $context->bem(false)
+            ->tag('link');
+});
+
+$engine->registerMatcher('page__favicon', function (ContextInterface $context) {
+    $context->bem(false)
+            ->tag('link')
+            ->attrs('rel', 'shortcut icon');
 });
